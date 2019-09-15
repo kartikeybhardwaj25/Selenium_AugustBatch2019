188a50424cd948f648ff4a17b3bbc1eac20b306d
kartikeybhardwaj25 <38663572+kartikeybhardwaj25@users.noreply.github.com>
2019-09-15 21:04:27 +0530

    committed

---
+++ HeadLessBrowser_example/src/HeadlessBrowser.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class HeadlessBrowser {
+
+	public static void main(String[] args) {
+		// System.setProperty("webdriver.chrome.driver", "C:\\Selenium_Jars\\ChromeDriver\\chromedriver.exe");
+		
+		
+		// WebDriver driver = new ChromeDriver();
+		WebDriver driver = new HtmlUnitDriver();
+		 
+		 driver.get("http://google.com");
+		 
+		 WebElement element = driver.findElement(By.name("q"));
+		 
+		 element.sendKeys("edureka");
+		 
+		 element.submit();
+		 
+		 System.out.println("Page title for headless browser:    " +driver.getTitle());
+		 
+		 driver.quit();
+
+	}
+
+}
